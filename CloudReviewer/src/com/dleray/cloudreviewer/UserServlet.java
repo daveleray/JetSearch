2f2e1405566e1fa7c4d9c7e6983fbd2880b944b5
dleray <daveleray@gmail.com>
2014-09-24 23:07:59 -0400

    c2 test

--- CloudReviewer/src/com/dleray/cloudreviewer/UserServlet.java
+++ CloudReviewer/src/com/dleray/cloudreviewer/UserServlet.java
@@ -2,11 +2,6 @@ package com.dleray.cloudreviewer;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.dleray.cloudreviewer.structures.CloudReviewerUser;
 import com.dleray.cloudreviewer.structures.CloudReviewerUserEndpoint;
 
